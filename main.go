da65a9a20f74d815eb77897029ab3271f6682175
Ahmad Nadil <88835532+IceTeaXXD@users.noreply.github.com>
2023-07-14 22:48:57 +0700

    chore: debug cors

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ func main() {
 	r.Use(cors.New(cors.Config{
 		AllowOrigins: []string{"https://chatakudong.vercel.app","http://localhost:3000", "https://ohl-fe.vercel.app"},
 		AllowMethods: []string{"GET", "POST", "DELETE", "PUT", "OPTIONS"},
-		AllowHeaders: []string{"Origin", "Content-Type"},
+		AllowHeaders: []string{"Origin", "Content-Type", "Authorization", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Access-Control-Allow-Methods", "Access-Control-Allow-Credentials", "Access-Control-Allow-Headers", "Accept", "Accept-Encoding", "Content-Length", "X-CSRF-Token", "Authorization"},
 	}))
 
 	
