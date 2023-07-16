edabd328c8dd636e18a2d9482f323fba7345730b
Ahmad Nadil <88835532+IceTeaXXD@users.noreply.github.com>
2023-07-16 19:23:39 +0700

    refactor: backend endpoint

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
 
 	// Add CORS middleware
 	r.Use(cors.New(cors.Config{
-		AllowOrigins: []string{"http://localhost:3000", "https://ohl-fe.vercel.app","http://localhost:5173"},
+		AllowOrigins: []string{"http://localhost:3000", "https://ohl-fe.vercel.app","http://localhost:5173","https://monolith-production-63a6.up.railway.app"},
 		AllowMethods: []string{"GET", "POST", "DELETE", "PUT", "OPTIONS"},
 		AllowHeaders: []string{"Origin", "Content-Type", "Authorization", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Access-Control-Allow-Methods", "Access-Control-Allow-Credentials", "Access-Control-Allow-Headers", "Accept", "Accept-Encoding", "Content-Length", "X-CSRF-Token", "Authorization"},
 	}))
