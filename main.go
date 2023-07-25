363d64c129f255220d8d1c3f5ac368cddc33e3b3
Ahmad Nadil <88835532+IceTeaXXD@users.noreply.github.com>
2023-07-25 20:39:49 +0700

    refactor: get barang is now an unauthorized function

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ func main() {
 
 	// Add CORS middleware
 	r.Use(cors.New(cors.Config{
-		AllowOrigins: []string{"http://localhost:3000", "https://ohl-fe.vercel.app", "http://localhost:5173", "https://monolith-labpro.up.railway.app/"},
+		AllowOrigins: []string{"http://localhost:3000", "https://ohl-fe.vercel.app", "http://localhost:5173", "https://monolith-labpro.up.railway.app/", "http://127.0.0.1:8000/"},
 		AllowMethods: []string{"GET", "POST", "DELETE", "PUT", "OPTIONS"},
 		AllowHeaders: []string{"Origin", "Content-Type", "Authorization", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Access-Control-Allow-Methods", "Access-Control-Allow-Credentials", "Access-Control-Allow-Headers", "Accept", "Accept-Encoding", "Content-Length", "X-CSRF-Token", "Authorization"},
 	}))
@@ -44,6 +44,8 @@ func main() {
 
 	r.POST("/login", controllers.Login)
 	r.POST("/register", controllers.Register)
+	r.GET("/barang", controllers.GetBarang)
+	r.GET("/barang/:id", controllers.GetBarangByID)
 	r.OPTIONS("/login", controllers.LoginOptions)
 
 	r.Run()
