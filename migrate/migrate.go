632076924e3042d1b7efb32682bc24abb36fc96d
Ahmad Nadil <88835532+IceTeaXXD@users.noreply.github.com>
2023-07-28 10:26:15 +0700

    feat: seeding

--- migrate/migrate.go
+++ migrate/migrate.go
@@ -1,8 +1,9 @@
-package main
+package migrate
 
 import (
 	"singleservice/initializers"
 	model "singleservice/models"
+	"fmt"
 )
 
 func init() {
@@ -10,6 +11,21 @@ func init() {
 	initializers.ConnectToDB()
 }
 
-func main() {
-	initializers.DB.AutoMigrate(&model.User{}, &model.Perusahaan{}, &model.Barang{})
+func Migrate(){
+	fmt.Println("Migrating........................")
+	if !initializers.DB.Migrator().HasTable(&model.User{}) {
+		fmt.Println("Migrating User........................")
+		initializers.DB.AutoMigrate(&model.User{})
+		initializers.DB.Create(&model.User{ Username: "admin", Password: "admin", Name: "Administrator", IsAdmin: true })
+	}
+	if !initializers.DB.Migrator().HasTable(&model.Perusahaan{}) {
+		fmt.Println("Migrating Perusahaan........................")
+		initializers.DB.AutoMigrate(&model.Perusahaan{})
+		initializers.DB.Create(&model.Perusahaan{ Nama: "PT. Maju Mundur", Alamat: "Jl. Maju Mundur No. 1", NoTelepon: "08123456789", KodePajak: "MU1" })
+	}
+	if !initializers.DB.Migrator().HasTable(&model.Barang{}) {
+		fmt.Println("Migrating Barang........................")
+		initializers.DB.AutoMigrate(&model.Barang{})
+		initializers.DB.Create(&model.Barang{ Nama: "Beras", Harga: 10000, Stok: 100, PerusahaanID: "1", KodeBarang: "BR002" })
+	}
 }
